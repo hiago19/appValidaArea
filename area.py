4d9b43664d56b8727562eca7ebc7265e31b10f04
bhiago19 <test@example.com>
2024-05-11 11:43:03 -0300

    Initial commit

---
+++ area.py
@@ -0,0 +1,20 @@
+class Retangulo:
+    def __init__(self, lado_a, lado_b):
+        self.a = lado_a
+        self.b = lado_b
+
+    def muda_valor(self, novo_a, novo_b):
+        self.a = novo_a
+        self.b = novo_b
+
+    def retorna_lado(self):
+        print(f'O Retângulo possui dimensões {self.a}m x {self.b}m')
+
+    def area(self):
+        return self.a * self.b
+
+# Exemplo de uso da classe Retangulo
+if __name__ == "__main__":
+    retangulo = Retangulo(5, 10)
+    retangulo.retorna_lado()
+    print("Área:", retangulo.area())
